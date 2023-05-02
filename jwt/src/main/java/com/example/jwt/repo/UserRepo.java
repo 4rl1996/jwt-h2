e907e14bb8f3220d8508cdcc62f4d0dda9d0c028
Roman <you@example.com>
2023-05-03 00:42:18 +0300

    prereq

---
+++ jwt/src/main/java/com/example/jwt/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.example.jwt.repo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo {
+}
